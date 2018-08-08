025dfcd554091364db977c5f4a1ca6759e9315c7
r.biffi <biffi.riccardo91@gmail.com>
2018-08-09 01:47:08 +0200

    Inserimento dati nel fragment (spostando tutti i metodi dati-adapter dentro il fragment). Visualizzazione scrollabile della lista.

--- app/src/main/java/com/rbiffi/vacationfriend/VacationList/Recenti.java
+++ app/src/main/java/com/rbiffi/vacationfriend/VacationList/Recenti.java
@@ -6,18 +6,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.rbiffi.vacationfriend.R;
 
-public class Recenti extends Fragment {
+import java.util.ArrayList;
+
+public class Recenti extends Fragment{
+
+    private ListView vacationList;
+    private ArrayList dataSource;
+    private ArrayAdapter dataAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // non posso passare il layout con setContentView, devo usare i parametri in input
         // l'inflater trasforma xml in codice java a run time
-        return inflater.inflate(R.layout.fragment_recenti, container, false);
         //container è il padre del fragment, si arrangia lui a sostituirlo
+        return inflater.inflate(R.layout.fragment_recenti, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // uso un adapter per recuperare i dati da una sorgente e posizionarli nelle giuste posizioni dell'interfaccia
+        // è l'intermediario che visualizza i dati
+        // tipo l'array adapter ha per sorgente un array. (a me serve sia salvarli che leggerli)
+        dataSource = new ArrayList();
+        dataSource.add("Oggetto 1");
+        dataSource.add("Oggetto 2");
+        dataSource.add("Oggetto 3");
+        dataSource.add("Oggetto 1");
+        dataSource.add("Oggetto 2");
+        dataSource.add("Oggetto 3");
+        dataSource.add("Oggetto 1");
+        dataSource.add("Oggetto 2");
+        dataSource.add("Oggetto 3");
+        dataSource.add("Oggetto 1");
+        dataSource.add("Oggetto 2");
+        dataSource.add("Oggetto 3");
+        dataSource.add("Oggetto 1");
+        dataSource.add("Oggetto 2");
+        dataSource.add("Oggetto 3");
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        vacationList = getView().findViewById(R.id.vacationElList);
+        vacationList.setDivider(null); // rimuovo il divisore dalle liste (perché noi l'abbiamo fatto interno all'elemento
+
+        // gli dico all'adapter dove sono i dati (source) e dove metterli (layout, elemento)
+        dataAdapter = new ArrayAdapter(getContext(), R.layout.vacation_list_row, R.id.rowText, dataSource);
+        vacationList.setAdapter(dataAdapter); // connetto la lista e l'adapter
     }
 
     // logica del frammento qui sotto
