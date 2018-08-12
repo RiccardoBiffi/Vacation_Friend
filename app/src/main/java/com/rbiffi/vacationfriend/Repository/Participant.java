0f80c826748c1fc8f57d1a3aa9f6d67a0d330317
r.biffi <biffi.riccardo91@gmail.com>
2018-08-12 03:06:06 +0200

    Creazione scheletro di DAO e Database. Sto facendo leggendo la guida per fare il repository. Prevedo problemi per tipi di dati particolari come date, immagini e liste di oggetti (relazioni).

--- app/src/main/java/com/rbiffi/vacationfriend/Repository/Participant.java
+++ app/src/main/java/com/rbiffi/vacationfriend/Repository/Participant.java
@@ -4,15 +4,23 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 
 @Entity(indices = {@Index("email")})
 public class Participant {
 
     @PrimaryKey
+    @NonNull
     public String email;
 
+    public int vacationId;
+
+    @NonNull
     public String name;
+    @NonNull
     public String lastName;
-    public Bitmap picture;
+
+    //@NonNull
+    //public Bitmap picture;
 
 }
--- app/src/main/java/com/rbiffi/vacationfriend/Repository/Vacation.java
+++ app/src/main/java/com/rbiffi/vacationfriend/Repository/Vacation.java
@@ -1,9 +1,14 @@
 package com.rbiffi.vacationfriend.Repository;
 
+import android.arch.persistence.room.Embedded;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.Relation;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
 import java.sql.Date;
 import java.util.List;
 
@@ -11,12 +16,22 @@ import java.util.List;
 public class Vacation {
 
     @PrimaryKey(autoGenerate = true)
+    @NonNull
     public int id;
 
+    @NonNull
     public String name;
+
+    //todo i tipi di dato commentati vanno convertiti in qualche modo prima di essere usati.
+    // forse si può fare lo stesso con colonne contenenti liste di oggetti
+    /*@NonNull
     public Date startDate;
+    @NonNull
     public Date endDate;
-    public List<Participant> participants;
+
+    //@Relation(parentColumn = "id", entityColumn = "vacationId")
+    //public List<Participant> participants;
     public Bitmap photo;
+    */
     public String note;
 }
