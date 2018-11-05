10a78ac050e51f13fdedde85d561f09c76cb2a18
r.biffi <biffi.riccardo91@gmail.com>
2018-11-05 01:26:34 +0100

    Creata sezione lista personale e collaborativa (solo tutorial, no nuova lista). Manca lista per tutti, con esempi di liste completate e non.
    Signed-off-by: r.biffi <biffi.riccardo91@gmail.com>

--- app/src/main/java/com/rbiffi/vacationfriend/AppSections/Liste/FragmentListPersonal.java
+++ app/src/main/java/com/rbiffi/vacationfriend/AppSections/Liste/FragmentListPersonal.java
@@ -2,23 +2,35 @@ package com.rbiffi.vacationfriend.AppSections.Liste;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.rbiffi.vacationfriend.R;
 
 public class FragmentListPersonal extends Fragment {
 
+    private FloatingActionButton floatingActionButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lists_personal, container, false);
+        return inflater.inflate(R.layout.empty_lists_personal, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        floatingActionButton = view.findViewById(R.id.lists_personal_floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "New personal list activity", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
